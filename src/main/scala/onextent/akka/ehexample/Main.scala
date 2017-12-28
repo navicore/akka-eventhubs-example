c65211ad4ca1ce1e375a98e251f90c6079080112
Ed Sweeney <ed@onextent.com>
2017-12-28 12:56:32 -0800

    restart

--- src/main/scala/onextent/akka/ehexample/Main.scala
+++ src/main/scala/onextent/akka/ehexample/Main.scala
@@ -1,6 +1,7 @@
 package onextent.akka.ehexample
 
-import akka.stream.scaladsl.Source
+import scala.concurrent.duration._
+import akka.stream.scaladsl.{RestartSource, Source}
 import onextent.akka.eventhubs.Eventhubs
 import onextent.akka.eventhubs.Conf._
 
@@ -10,9 +11,28 @@ object Main extends App {
 
   val mySource = Source.fromGraph(sourceGraph)
 
-  mySource.runForeach(m => {
+//  mySource.runForeach(m => {
+//    println(s"ejs yay: ${m._1.substring(0, 160)}")
+//    m._2.ack()
+//  })
 
-    println(s"ejs yay: ${m._1.substring(0, 160)}")
+  val restartSource = RestartSource.withBackoff(
+    minBackoff = 3.seconds,
+    maxBackoff = 30.seconds,
+    randomFactor = 0.2 // adds 20% "noise" to vary the intervals slightly
+  ) { () =>
+    // Create a source from a future of a source
+    mySource
+  }
+
+
+  //mySource.runForeach(m => {
+  restartSource.runForeach(m => {
+    val printme = m._1.length match {
+      case l if l <= 160 => m._1
+      case _ => m._1.substring(0, 160)
+    }
+    println(s"ejs yay: $printme")
     m._2.ack()
   })
 
