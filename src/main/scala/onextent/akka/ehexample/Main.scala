94c3e658cfb2c57f8a8bcc60645d37611379cd20
Ed Sweeney <ed@onextent.com>
2018-01-05 07:30:14 -0800

    works

--- src/main/scala/onextent/akka/ehexample/Main.scala
+++ src/main/scala/onextent/akka/ehexample/Main.scala
@@ -11,29 +11,29 @@ object Main extends App {
 
   val mySource = Source.fromGraph(sourceGraph)
 
-//  mySource.runForeach(m => {
-//    println(s"ejs yay: ${m._1.substring(0, 160)}")
-//    m._2.ack()
-//  })
-
-  val restartSource = RestartSource.withBackoff(
-    minBackoff = 3.seconds,
-    maxBackoff = 30.seconds,
-    randomFactor = 0.2 // adds 20% "noise" to vary the intervals slightly
-  ) { () =>
-    // Create a source from a future of a source
-    mySource
-  }
-
-
-  //mySource.runForeach(m => {
-  restartSource.runForeach(m => {
-    val printme = m._1.length match {
-      case l if l <= 160 => m._1
-      case _ => m._1.substring(0, 160)
-    }
-    println(s"ejs yay: $printme")
+  mySource.runForeach(m => {
+    println(s"ejs yay: ${m._1.substring(0, 160)}")
     m._2.ack()
   })
+
+//  val restartSource = RestartSource.withBackoff(
+//    minBackoff = 3.seconds,
+//    maxBackoff = 30.seconds,
+//    randomFactor = 0.2 // adds 20% "noise" to vary the intervals slightly
+//  ) { () =>
+//    // Create a source from a future of a source
+//    mySource
+//  }
+//
+//
+//  //mySource.runForeach(m => {
+//  restartSource.runForeach(m => {
+//    val printme = m._1.length match {
+//      case l if l <= 160 => m._1
+//      case _ => m._1.substring(0, 160)
+//    }
+//    println(s"ejs yay: $printme")
+//    m._2.ack()
+//  })
 
 }
