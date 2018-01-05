dfb3b9713f711aaa3a89eb17c8ba170c47776c0e
Ed Sweeney <ed@onextent.com>
2018-01-05 07:21:52 -0800

    working

--- src/main/scala/onextent/akka/ehexample/Main.scala
+++ src/main/scala/onextent/akka/ehexample/Main.scala
@@ -2,12 +2,12 @@ package onextent.akka.ehexample
 
 import scala.concurrent.duration._
 import akka.stream.scaladsl.{RestartSource, Source}
-import onextent.akka.eventhubs.{Eventhubs, InputEventHubConf1}
+import onextent.akka.eventhubs._
 import onextent.akka.eventhubs.Conf._
 
 object Main extends App {
 
-  val sourceGraph = new Eventhubs(InputEventHubConf1)
+  val sourceGraph = new Eventhubs()
 
   val mySource = Source.fromGraph(sourceGraph)
 
