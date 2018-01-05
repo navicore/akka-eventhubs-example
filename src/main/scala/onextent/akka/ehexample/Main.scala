6bd6f63a948cf54983a1dc1761ac83c3cc80c522
Ed Sweeney <ed@onextent.com>
2018-01-05 07:17:16 -0800

    debugging

--- src/main/scala/onextent/akka/ehexample/Main.scala
+++ src/main/scala/onextent/akka/ehexample/Main.scala
@@ -1,21 +1,28 @@
 package onextent.akka.ehexample
 
-import scala.concurrent.duration._
-import akka.stream.scaladsl.{RestartSource, Source}
-import onextent.akka.eventhubs.{Eventhubs, InputEventHubConf1}
+import akka.stream.scaladsl.Source
 import onextent.akka.eventhubs.Conf._
+import onextent.akka.eventhubs.{Eventhubs, InputEventHubConf1, InputEventHubConf2}
 
 object Main extends App {
 
   val sourceGraph = new Eventhubs(InputEventHubConf1)
+ // val sourceGraph2 = new Eventhubs(InputEventHubConf2)
 
   val mySource = Source.fromGraph(sourceGraph)
+  //val mySource2 = Source.fromGraph(sourceGraph2)
 
-//  mySource.runForeach(m => {
-//    println(s"ejs yay: ${m._1.substring(0, 160)}")
+  mySource.runForeach(m => {
+    println(s"source1: ${m._1.substring(0, 160)}")
+    m._2.ack()
+  })
+
+//  mySource2.runForeach(m => {
+//    println(s"source2: ${m._1.substring(0, 160)}")
 //    m._2.ack()
 //  })
 
+  /*
   val restartSource = RestartSource.withBackoff(
     minBackoff = 3.seconds,
     maxBackoff = 30.seconds,
@@ -24,7 +31,6 @@ object Main extends App {
     // Create a source from a future of a source
     mySource
   }
-
 
   //mySource.runForeach(m => {
   restartSource.runForeach(m => {
@@ -35,5 +41,6 @@ object Main extends App {
     println(s"ejs yay: $printme")
     m._2.ack()
   })
+   */
 
 }
