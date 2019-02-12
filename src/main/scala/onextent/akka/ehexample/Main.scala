49b868b595ff0557c6afe94c393e80ccce082c9c
Ed Sweeney <ed@onextent.com>
2019-02-12 07:18:33 -0800

    logging

--- src/main/scala/onextent/akka/ehexample/Main.scala
+++ src/main/scala/onextent/akka/ehexample/Main.scala
@@ -1,6 +1,6 @@
 package onextent.akka.ehexample
 
-import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.{Done, NotUsed}
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.LazyLogging
@@ -111,6 +111,63 @@ object SourceSinkExample extends LazyLogging {
         .runWith(new EventhubsSink(EventHubConf(outConfig)))
 
     }
+  }
+
+}
+
+object SourceSinkSinglePartitionExample extends LazyLogging {
+
+  val partitionId: Int = {
+    sys.env.get("POD_NAME") match {
+      case Some(str) =>
+        val pos = str.lastIndexOf('-')
+        str.substring(pos + 1).toInt
+      case _ => throw new java.lang.IllegalArgumentException("no pid in POD_NAME")
+    }
+  }
+
+  def apply(): Unit = {
+
+    val inConfig: Config = ConfigFactory.load().getConfig("eventhubs-in")
+    val outConfig: Config = ConfigFactory.load().getConfig("eventhubs-out")
+
+    val pid = partitionId
+
+    val src: Source[(String, AckableOffset), NotUsed] =
+      createPartitionSource(pid, inConfig)
+
+    val flow =
+      Flow[(String, AckableOffset)].map((x: (String, AckableOffset)) => {
+        if (conf.getBoolean("main.pretty") && x._1.charAt(0) == '{') {
+          import org.json4s._
+          import org.json4s.native.JsonMethods._
+          val parsedJson: JValue = parse(x._1)
+          println(
+            s"consumer pid $pid received:\n${compact(render(parsedJson))}")
+        } else {
+          println(s"consumer pid $pid received:\n${x._1}")
+        }
+        x
+      })
+
+    val format = Flow[(String, AckableOffset)].map(
+      (x: (String, AckableOffset)) =>
+        // pass the payload, the partition key, props, and ackable
+        EventhubsSinkData(x._1.getBytes("UTF8"),
+                          Some(x._2.ackme.partitionKey),
+                          Some(x._2.ackme.properties.toMap),
+                          Some(x._2)))
+
+    src
+      .via(flow)
+      .via(format)
+      .recover {
+        case e =>
+          logger.error(s"recover op caught ${e.getMessage}", e)
+          throw e
+      }
+      .runWith(new EventhubsSink(EventHubConf(outConfig)))
+
   }
 
 }
